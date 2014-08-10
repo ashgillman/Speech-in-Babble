4173f9123aaa141857d63a6bf03360803e73dadf
Ashley Gillman <gillwoman@gmail.com>
2014-08-11 09:02:35 +1000

    added 2 nmf libs. Continuing with Wilson algorithm, but can't make sense of the covariance concatenation described in the paper...

--- Code/MATLAB/Wilson/WilsonTest.m
+++ Code/MATLAB/Wilson/WilsonTest.m
@@ -4,6 +4,7 @@ clear all;
 Fs = 16000;
 nf = 513;
 nb = 80;
+nsLvl = 0.1;
 TEST_ID = 'alphatesting';
 SPKRS = {'c3c','c3f'}; % Speaker IDs used in this test
 REC_VALS = ['2':'9' 'a':'z']; % 34 recordings. Ignore '1', it's silence
@@ -22,17 +23,25 @@ PESQ_LOC = '/Users/Ash/Dropbox/Uni/2014/Thesis/Code/MATLAB/pesq';
 if isempty(strfind(path,PESQ_LOC))
     path(PESQ_LOC,path)
 end
+NMFLIB_LOC = '/Users/Ash/Dropbox/Uni/2014/Thesis/Code/MATLAB/nmflib';
+if isempty(strfind(path,NMFLIB_LOC))
+    path(NMFLIB_LOC,path)
+end
 
-%% Training
-% Form SoI training data
+%% Data retrieval and training
 trainWav = cell(size(SPKRS));
 spkrData = cell(size(SPKRS));
-X_spkr = cell(size(SPKRS));
-V_spkr = cell(size(SPKRS));
-W_spkr = cell(size(SPKRS));
+X_train = cell(size(SPKRS));
+V_train = cell(size(SPKRS));
+W_train = cell(size(SPKRS));
+testWav = cell(size(SPKRS));
+X_test = cell(size(SPKRS));
+V_test = cell(size(SPKRS));
+W_test = cell(size(SPKRS));
 figure()
 set(gca, 'LooseInset', get(gca,'TightInset'))
 for spkrI = 1:numel(SPKRS)
+    % Training data
     speaker = SPKRS{spkrI};
     wav = trainWav{spkrI};
     spkrData{spkrI} = getfromifo( ...
@@ -41,8 +50,8 @@ for spkrI = 1:numel(SPKRS)
     sex = spkrData{spkrI}.Talker_Sex(1); % f or m
     fprintf(    ['Training on speaker %3s (%2s %1s):' ...
         blanks(numel(ROOT_LOC)-10) '| start |  end  |\n'],speaker,age,sex)
-    for recIndex1 = 1:8 % first 8 for training
-        recNum = REC_VALS(recIndex1);
+    for recIndex = 1:8 % first 8 for training
+        recNum = REC_VALS(recIndex);
         [rec,FsIn] = audioread(sprintf('%s%s/%sa010%s.wav',ROOT_LOC, ...
             speaker,speaker,recNum));
         rec = resample(rec,FsIn,Fs);
@@ -54,12 +63,12 @@ for spkrI = 1:numel(SPKRS)
     trainWav{spkrI} = wav;
 
     % STFT
-    frameN = (nf-1)*2;
+    frameN = (nf-1)*2; % frame length (no. samples)
     win = hamming(frameN,'periodic');
     trainSTFT = rfft(enframe(wav,win,nf-1),frameN,2); % 50% overlap
-    X_spkr{spkrI} = abs(trainSTFT)';
+    X_train{spkrI} = abs(trainSTFT)';
     subplot(numel(SPKRS),2,2*spkrI-1);
-    surf(log10(X_spkr{spkrI}),'EdgeColor','none');
+    surf(log10(X_train{spkrI}),'EdgeColor','none');
     axis xy; axis tight; view(0,90);
     title(sprintf('%s: X \rightarrow Input Signal',speaker));
 
@@ -67,43 +76,59 @@ for spkrI = 1:numel(SPKRS)
     %soundsc(rev,Fs)
 
     % Training
-    [Vspeech,Wspeech] = nnmf(X_spkr{spkrI},nb);
+    [V,W] = nnmf(X_train{spkrI},nb);
+    V_train{spkrI} = V; W_train{spkrI} = W;
     subplot(numel(SPKRS),2,2*spkrI);
-    surf(log10(Vspeech*Wspeech),'EdgeColor','none');
+    surf(log10(V*W),'EdgeColor','none');
     axis xy; axis tight; view(0,90);
-    title('V_{all} \times W_{all} \rightarrow Approximation by factorisation');
+    title(['V_{all} \times W_{all} \rightarrow Approximation by ' ...
+        'factorisation']);
 
-    recovered = overlapadd(irfft((Vspeech*Wspeech)',frameN,2),win,nf-1);
+    %recovered = overlapadd(irfft((V*W)',frameN,2),win,nf-1);
     %soundsc(recovered,Fs)
+
+    % Test Data
+    recIndex = recIndex + 1;
+    recNum = REC_VALS(recIndex);
+    [rec,FsIn] = audioread(sprintf('%s%s/%sa010%s.wav',ROOT_LOC, ...
+        speaker,speaker,recNum));
+    testWav{spkrI} = resample(rec,FsIn,Fs);
 end
 
-%% Test Data
-recIndex1 = recIndex1 + 1;
-recNum = REC_VALS(recIndex1);
-[rec,FsIn] = audioread(sprintf('%s%sa010%s.wav',SOI_LOC,speaker,recNum));
-desired = resample(rec,FsIn,Fs);
+speech = testWav{1}; noise = testWav{2};
+Vspeech = V_train{1}; Vnoise = V_train{2};
+Wspeech = W_train{1}; Wnoise = W_train{2};
 
-recIndex1 = recIndex2 + 1;
-recNum = REC_VALS(recIndex2);
-[rec,FsIn] = audioread(sprintf('%s%sa010%s.wav',COMP_LOC,compspk,recNum));
-noise = resample(rec,FsIn,Fs);
-
-if numel(desired) > numel(noise)
-    noise = [noise;zeros(numel(desired)-numel(noise),1)];
+% get speech and noise same length
+if numel(speech) > numel(noise)
+    noise = [noise;zeros(numel(speech)-numel(noise),1)];
 else
-    desired = [desired;zeros(numel(noise)-numel(desired),1)];
+    speech = [speech;zeros(numel(noise)-numel(speech),1)];
 end
 
-mixed = desired + 0.1*noise;
+%% Statistics
+mu_speech = mean(log(Wspeech),2); mu_noise = mean(log(Wnoise),2);
+mu_all = [mu_speech;mu_noise];
+Lambda_speech = cov(log(Wspeech)'); Lambda_noise = cov(log(Wnoise)');
+%Lambda_all = [Lambda_speech 0;0 Lambda_noise];
+
+%% Mix
+mixed = speech + nsLvl*noise;
 soundsc(mixed,Fs)
 
-%% Extract
-%estimate = 
+frameN = (nf-1)*2; % frame length (no. samples)
+win = hamming(frameN,'periodic');
+sigSTFT = rfft(enframe(mixed,win,nf-1),frameN,2); % 50% overlap
+X_sig = abs(sigSTFT)';
+
+%% Denoise
+Vall = [Vspeech Vnoise];
+%targetEst = 
 
 before = 'before.wav';
 after = 'after.wav';
-audiowrite(before,trainWav,Fs)
-audiowrite(after,recovered,Fs)
+audiowrite(before,mixed,Fs)
+%audiowrite(after,recovered,Fs)
 
 [pesq_mos] = pesq(Fs,after,before)
     
